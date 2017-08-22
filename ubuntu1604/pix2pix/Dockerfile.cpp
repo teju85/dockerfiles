6b90e4e55fa4eeafb7c644b5fe899f4e70aef75e
Thejaswi Rao <rao.thejaswi@gmail.com>
2017-08-22 03:48:51 -0700

    pushed the pix2pix repo inside the image

--- ubuntu1604/pix2pix/Dockerfile.cpp
+++ ubuntu1604/pix2pix/Dockerfile.cpp
@@ -16,9 +16,8 @@ RUN apt-get update && \\
     rm -rf /var/lib/apt/lists && \\
     mkdir /var/lib/apt/lists
 
-// TODO: dataset downloader scripts currently download everything inside
-// the 'datasets' directory. Thus, the idea is to have custom scripts to
-// download the datasets into a volume mount!
-//RUN git clone "https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix"
-//WORKDIR pytorch-CycleGAN-and-pix2pix
-COPY downloader.sh /downloader.sh
+// for visdom
+EXPOSE 8097
+
+RUN git clone "https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix" "/pytorch-CycleGAN-and-pix2pix"
+WORKDIR "/pytorch-CycleGAN-and-pix2pix"
