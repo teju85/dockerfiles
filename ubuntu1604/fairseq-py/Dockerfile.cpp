3f05a8b86ab03d8bfffa5b8ea2862cc7603d9c35
Thejaswi Rao <rao.thejaswi@gmail.com>
2017-09-25 04:32:25 -0700

    fixed the hack to resolve unicode error while installing fairseq-py

--- ubuntu1604/fairseq-py/Dockerfile.cpp
+++ ubuntu1604/fairseq-py/Dockerfile.cpp
@@ -8,9 +8,18 @@ RUN git clone "https://github.com/facebookresearch/fairseq-py" /opt/fairseq-py
 
 RUN cd /opt/fairseq-py && \\
     pip install --no-cache-dir -r requirements.txt && \\
-    grep -v Pourquoi README.md > README.md.bak && \\
-    mv README.md.bak README.md && \\
-    python setup.py build && \\
-    python setup.py develop
+    LANG=C.UTF-8 python setup.py build && \\
+    LANG=C.UTF-8 python setup.py develop
+
+RUN cd /opt/fairseq-py/data && \\
+    bash ./prepare-iwslt14.sh && \\
+    cd .. && \\
+    export INTEXT=data/iwslt14.tokenized.de-en && \\
+    export OUTTEXT=data-bin/iwslt14.tokenized.de-en && \\
+    export LANG=C.UTF-8 && \\
+    python preprocess.py --source-lang de --target-lang en \\
+        --trainpref $INTEXT/train --validpref $INTEXT/valid \\
+        --testpref $INTEXT/test --thresholdtgt 3 --thresholdsrc 3 \\
+        --destdir $OUTTEXT
 
 WORKDIR /opt/fairseq-py
