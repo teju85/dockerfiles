dc6f550742f8559f153937f0b6deaa40e649d1a4
Thejaswi Rao <rao.thejaswi@gmail.com>
2017-10-05 02:03:10 -0700

    1. Fixed indentation in pyml 2. Updates to pytorch/fairseq-py 3. Added a helper target to Makefile to ease context creation

--- ubuntu1604/fairseq-py/Dockerfile.cpp
+++ ubuntu1604/fairseq-py/Dockerfile.cpp
@@ -1,22 +1,25 @@
 FROM cudnn:6.0
 
 #define PT_COMMIT_ID a03e5cb40938b6b3f3e6dbddf9cff8afdff72d1b
+#define EXTRA_TORCH_FLAGS NO_DISTRIBUTED=1
 #include "../../reusables/pytorch"
+
 #include "../../reusables/jupyter"
 
 RUN git clone "https://github.com/facebookresearch/fairseq-py" /opt/fairseq-py
 
+ENV LANG=C.UTF-8
+ENV INTEXT=/opt/fairseq-py/data/iwslt14.tokenized.de-en
+ENV OUTTEXT=/opt/fairseq-py/data-bin/iwslt14.tokenized.de-en
+
 RUN cd /opt/fairseq-py && \\
     pip install --no-cache-dir -r requirements.txt && \\
-    LANG=C.UTF-8 python setup.py build && \\
-    LANG=C.UTF-8 python setup.py develop
+    python setup.py build && \\
+    python setup.py develop
 
 RUN cd /opt/fairseq-py/data && \\
     bash ./prepare-iwslt14.sh && \\
     cd .. && \\
-    export INTEXT=data/iwslt14.tokenized.de-en && \\
-    export OUTTEXT=data-bin/iwslt14.tokenized.de-en && \\
-    export LANG=C.UTF-8 && \\
     python preprocess.py --source-lang de --target-lang en \\
         --trainpref $INTEXT/train --validpref $INTEXT/valid \\
         --testpref $INTEXT/test --thresholdtgt 3 --thresholdsrc 3 \\
