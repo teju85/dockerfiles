311c1c20d56f29a0a0bc097f709dc53629aeec0f
Thejaswi Rao <rao.thejaswi@gmail.com>
2017-12-12 05:53:36 -0800

    Added cuda9 dockerfile + refactored common parts of cuda8 and cuda9

--- ubuntu1604/cuda80/Dockerfile.cpp
+++ ubuntu1604/cuda80/Dockerfile.cpp
@@ -3,4 +3,4 @@ FROM ubuntu:16.04
 #define  __CUDA_VERSION_FULL  "8.0.61"
 #define  __CUDA_VERSION_PKG   "8-0=8.0.61-1"
 #define  __CUDA_VERSION_SHORT "8.0"
-#include "../../reusables/cuda-dev"
+#include "../../reusables/cuda"
---
+++ ubuntu1604/cuda90/Dockerfile.cpp
@@ -0,0 +1,6 @@
+FROM ubuntu:16.04
+
+#define  __CUDA_VERSION_FULL  "9.0.176"
+#define  __CUDA_VERSION_PKG   "9-0=9.0.176-1"
+#define  __CUDA_VERSION_SHORT "9.0"
+#include "../../reusables/cuda"
