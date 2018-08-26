5ff407c598b54e101635218bb55cfa5a991da623
Thejaswi Rao <rao.thejaswi@gmail.com>
2018-08-26 10:49:19 -0700

    added more dependencies to ml-dev

--- ubuntu1604/ml-dev/Dockerfile.cpp
+++ ubuntu1604/ml-dev/Dockerfile.cpp
@@ -2,10 +2,13 @@ FROM cuda:9.2-dev
 
 #include "../../reusables/cmake-build"
 #include "../../reusables/ssh"
-
+#include "../../reusables/python-dev-2.7"
 
 RUN apt-get update && \\
     apt-get install -y --no-install-recommends \\
+        gzip \\
+        tar \\
+        unzip \\
         zlib1g-dev && \\
     rm -rf /var/lib/apt/lists && \\
     mkdir /var/lib/apt/lists
