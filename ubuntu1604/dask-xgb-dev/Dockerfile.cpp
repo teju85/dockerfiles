d94248a258c590c9818f82087be75b36f4a64ea9
Thejaswi Rao <rao.thejaswi@gmail.com>
2018-09-02 21:11:01 -0700

    removed dev versions of xgboost and dask-xgboost, as they can be installed as user from the release versions of their corresponding containers

--- ubuntu1604/dask-xgb-dev/Dockerfile.cpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef __CUDA_VERSION_SHORT
-#define "__CUDA_VERSION_SHORT is a mandatory define! Eg: dev-8.0"
-#endif
-
-FROM xgb:__CUDA_VERSION_SHORT
-
-#include "../../reusables/dask-xgboost"
-#include "../../reusables/ssh"
