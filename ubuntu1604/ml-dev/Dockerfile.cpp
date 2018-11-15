3291c1be73df2985761967705567079fc6209446
Thejaswi Rao <rao.thejaswi@gmail.com>
2018-11-15 06:59:04 -0800

    1. Used pre-built clang/clang-format inside ml:dev 2. Updated conda installation process to pick python3 by default

--- ubuntu1604/ml-dev/Dockerfile.cpp
+++ ubuntu1604/ml-dev/Dockerfile.cpp
@@ -3,11 +3,13 @@ FROM cuda:10.0-dev
 #include "../../reusables/cmake-build"
 #include "../../reusables/ssh"
 #include "../../reusables/conda-ml-env"
-#include "../../reusables/llvm"
 
 RUN apt-get update && \\
     apt-get install -y --no-install-recommends \\
+        clang \\
+        clang-format \\
         gzip \\
+        libpthread-stubs0-dev \\
         tar \\
         unzip \\
         zlib1g-dev && \\
